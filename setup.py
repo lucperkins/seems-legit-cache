9892ca10651c8f5c2a4b12a5d5228c6bca844e36
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-07 14:24:19 -0700

    Move everything into __init__.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open('README.md').read() as long_desc:
+    long_description = long_desc
+
 setup(name='SeemsLegitCache',
       version='0.1.0',
       description="A dict-based cache that's probably fine to use",
-      long_description=open('README.md'),
+      long_description=long_description,
       author='Luc Perkins',
       author_email='lucperkins@gmail.com',
       license='MIT',
       packages=['seemslegitcache'],
-      include_package_data=True)
+      url='https://github.com/lucperkins/seems-legit-cache',
+      include_package_data=True,
+      zip_safe=True)
