f32033711f37204610ae367c22a6141fe06dcfc2
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-07 13:53:44 -0700

    Initial 0.1.0

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='SeemsLegitCache',
+      version='0.1.0',
+      description="A dict-based cache that's probably fine to use",
+      long_description=open('README.md'),
+      author='Luc Perkins',
+      author_email='lucperkins@gmail.com',
+      license='MIT',
+      packages=find_packages())
