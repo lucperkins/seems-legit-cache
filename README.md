e78d99f367bda1dc8449424c3f4ae4854ed8a86f
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-09 15:08:37 -0700

    Add PyPi badge
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Seems Legit Cache
 
+![PyPi badge](https://img.shields.io/pypi/v/seems-legit-cache?color=yellow&style=flat-square)
+
 A caching library that's probably pretty good. I dunno.
--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup
 
-with open('README.md').read() as long_desc:
-    long_description = long_desc
-
 setup(name='seems-legit-cache',
       version='0.1.0',
       description="A dict-based cache that's probably fine to use",
-      long_description=long_description,
+      long_description=open('README.md').read(),
       author='Luc Perkins',
       author_email='lucperkins@gmail.com',
       license='MIT',
